5be2e87971039d53833a103a3fad267d2c316e75
xhguo <xhguo@boya-triz.com>
2025-12-12 17:53:06 +0800

    fix: Revert back to original apps.py of Android

--- phone_agent/config/apps.py
+++ phone_agent/config/apps.py
@@ -2,10 +2,9 @@
 
 APP_PACKAGES: dict[str, str] = {
     # Social & Messaging
-    "微信": "com.tencent.xin",
+    "微信": "com.tencent.mm",
     "QQ": "com.tencent.mobileqq",
     "微博": "com.sina.weibo",
-    "钉钉": "com.laiwang.DingTalk",
     # E-commerce
     "淘宝": "com.taobao.taobao",
     "京东": "com.jingdong.app.mall",
@@ -182,8 +181,8 @@ APP_PACKAGES: dict[str, str] = {
     "twitter": "com.twitter.android",
     "X": "com.twitter.android",
     "VLC": "org.videolan.vlc",
-    "WeChat": "com.tencent.xin.ios",
-    "wechat": "com.tencent.xin.ios",
+    "WeChat": "com.tencent.mm",
+    "wechat": "com.tencent.mm",
     "Whatsapp": "com.whatsapp",
     "WhatsApp": "com.whatsapp",
 }
